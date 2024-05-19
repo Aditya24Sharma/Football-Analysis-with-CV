ca1067f73c80a256b1606adf5ae4164c16d1760d
Aditya Sharma <aditya2002.05.19@gmail.com>
2024-05-18 22:49:34 -0400

    Intro to CNN and YOLO

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 from ultralytics import YOLO
+
+
+#yolov8 is convinient than 9
+model = YOLO('yolov8l')
+
+results = model.predict('input_videos/08fd33_4.mp4', save = True)
+print(results[0])
+print(['x' for i in range(10)])
+for box in results[0].boxes:
+    print(box)
