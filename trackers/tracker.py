80fbb0cb07060605d7e2802b8a3790816863392e
Aditya Sharma <aditya2002.05.19@gmail.com>
2024-06-03 11:15:31 -0400

    tracker object creation & storage

--- trackers/tracker.py
+++ trackers/tracker.py
@@ -1,10 +1,13 @@
 from ultralytics import YOLO
-import supervision as sv 
+import supervision as sv  #supervision helps to seamlessly track the objects that our model detected 
+# import pickle
+# import os
+
 
 class Tracker:
     def __init__(self, model_path):
         self.model = YOLO(model_path)
-        self.tracker = sv.ByteTrack()
+        self.tracker = sv.ByteTrack()   #track objects motion and path by assigning them with tracker id
 
     def detect_frames(self, frames):
         batch_size = 20         #instead of detecting every frame we will detect in every 20 frame
@@ -15,12 +18,31 @@ class Tracker:
             break
         return detections
     
-    def get_object_track(self, frames):
+    def get_object_track(self, frames, read_from_stub=False, stub_path=None):
+        
+        # if read_from_stub and stub_path is not None and os.path.exists(stub_path):
+        #     with open(stub_path, 'wb') as f:
+        #         tracks = pickle.load(f)
+        #     return tracks
+
+
         detections = self.detect_frames(frames)
+
+        tracks = {
+            # object: [{frame1},{frame2}, {tracker_id:{bbox:[0,0,0,0]}}]
+            'players': [],
+            'referees': [],
+            'ball':[],
+        }
 
         for frame_num, detection in enumerate(detections):
             cls_names = detection.names
             cls_names_inv = {value:key for key, value in cls_names.items()}     #inversing the key and value pairs
+
+            #creating a new dictionary for each frame
+            tracks['players'].append({})
+            tracks['referees'].append({})
+            tracks['ball'].append({})
 
             #Convert to supervision Detection format
             detection_supervision = sv.Detections.from_ultralytics(detection)
@@ -31,6 +53,38 @@ class Tracker:
                     detection_supervision.class_id[obj_id] = cls_names_inv["player"]
 
             #Track Objects
+            #update_with_detections is used to hold the current position of the player and connect it with past trajectories
             detection_with_tracks = self.tracker.update_with_detections(detection_supervision)
+            
+            print('+'*70)
 
-            print(detection_supervision)
+            print(detection_with_tracks)
+
+            for frame_detections in detection_with_tracks:  #provides a list of the detections with bbox, cls, trk_id, etc for each detections in a frame
+
+                bbox = frame_detections[0].tolist()
+                cls_id = frame_detections[3]
+                tracker_id = frame_detections[4]
+
+
+                if cls_id == cls_names_inv['player']:
+                    tracks['players'][frame_num][tracker_id] = {'bbox':bbox}
+
+                if cls_id == cls_names_inv['referees']:
+                    tracks['players'][frame_num][tracker_id] = {'bbox':bbox}
+
+             
+            #We don't need to track the ball for its data therefore there is no need to use detection_with_tracks
+            for frame_detections in detection_supervision:
+                bbox = frame_detections[0].tolist()
+                cls_id = frame_detections[3]
+                tracker_id = frame_detections[4]
+
+                if cls_id == cls_names_inv['ball']:
+                    tracks['ball'][frame_num][1] = {'bbox':bbox} #no use of tracking id of a ball so we put just a constant
+
+            # if stub_path is not None:
+            #     with open(stub_path, 'wb') as f:
+            #         pickle.dump(tracks,f) 
+        
+        return tracks
