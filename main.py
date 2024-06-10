5a56f4112c9ac5531009389ecab4f532ec22e392
Aditya Sharma <aditya2002.05.19@gmail.com>
2024-06-10 19:54:54 -0400

    annotations

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from utils import read_video, save_video
 from trackers import Tracker
+import time
 
 def main():
     #read video
@@ -8,12 +9,27 @@ def main():
     #Initialize Tracker
     tracker = Tracker('models/best.pt')
 
+
+    print('Ran Tracker class')
+    
     tracks = tracker.get_object_track(video_frames, 
                                       read_from_stub=True,
                                       stub_path = 'stubs/track_stubs.pkl')
 
+
+    print('Ran get object track')
+    print(type(video_frames))
+    print(len(video_frames[0:300]))
+    time.sleep(1)
+
+    #Draw output
+    output_video_frames = tracker.draw_annotations(video_frames[0:300], tracks) 
+
+
+    print('got output_video_frames')
+
     #save video
-    save_video(video_frames, 'output_videos/output_video.avi')
+    save_video(output_video_frames, 'output_videos/output_video.avi')
 
 if __name__ == '__main__':
     main()
