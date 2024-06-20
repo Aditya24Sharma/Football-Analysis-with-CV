8b89d31a91fdb08e84e1c982ff4ffcfd8c2a1139
Aditya Sharma <aditya2002.05.19@gmail.com>
2024-06-20 11:44:49 -0400

    Color clustering 1

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from utils import read_video, save_video
 from trackers import Tracker
 import time
+import cv2
 
 def main():
     #read video
@@ -15,6 +16,18 @@ def main():
     tracks = tracker.get_object_track(video_frames, 
                                       read_from_stub=True,
                                       stub_path = 'stubs/track_stubs.pkl')
+
+    #save cropped image of a player
+    for track_id, player in tracks['players'][0].items():
+        bbox = player['bbox']
+        frame = video_frames[0]
+
+        #crop bbox from frame
+        cropped_image = frame[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])]
+
+        #save the cropped image
+        cv2.imwrite(f'output_videos/cropped_image.jpg', cropped_image)
+        break
 
 
     print('Ran get object track')
